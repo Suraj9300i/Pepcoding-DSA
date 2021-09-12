022b4d6949aad494e0c76f69d25a75883d84c0d0
suraj <srjgupta17@gmail.com>
2021-09-12 12:22:24 +0545

    testing

---
+++ Sample.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define ll long long
+#define mod 1000000007
+#define debug(x) cerr<<#x<<" :- ";_print(x);cerr<<endl;
+#define debugArray(x,y) cerr<<#x<<" :- ";_print(x,y);cerr<<endl;
+using namespace std;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+void fastIO(){
+	#ifndef ONLINE_JUDGE 
+	freopen("Error.txt","w",stderr);
+	#endif
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+}
+template<class T> void _print(T x){cerr<<x;}
+template<class T> void _print(vector<T> v){cerr<<"[ ";for(T ele:v) cerr<<ele<<" "; cerr<<"]\n";}
+template<class T> void _print(T arr[],int n){cerr<<"[ ";for(int i=0;i<n;i++) cerr<<arr[i]<<" "; cerr<<"]\n";}
+
+/***************************Jay Shree Ram****************************/
+int solve(){
+	int n;
+    cout<<(n*10);
+}
+
+int main(){
+	fastIO();
+	int t = 1;
+	// cin>>t;
+	solve(10);
+	for(int i=1;i<=t;i++){
+		
+	}
+
+return 0;
+}
