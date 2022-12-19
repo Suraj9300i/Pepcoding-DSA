c3fa55fbae36d393b1003fce1593786294f480d8
Suraj Gupta <48242153+Suraj9300i@users.noreply.github.com>
2022-12-20 00:28:08 +0530

    Update Sample.cpp

--- Sample.cpp
+++ Sample.cpp
@@ -36,3 +36,15 @@ int main(){
 
 return 0;
 }
+
+
+
+
+
+
+// Build System for CPP
+{
+	"cmd" : ["g++ -std=c++17 $file_name -o $file_base_name && timeout 4s ./$file_base_name<input.txt>output.txt"],"selector" : "source.c",
+	"shell":true,
+	"working_dir" : "$file_path"
+}
